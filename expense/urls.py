46e66532715f7793182b0fcbc9e97390e2ef78d1
Abramov Danil <abramovd200@gmail.com>
2025-01-21 15:58:38 +0300

    add templates

--- expense/urls.py
+++ expense/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 from . import views
+from .views import Register
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('django.contrib.auth.urls')),
+    path('/register', Register.as_view(), name='register'),
 ]
--- expense/views.py
+++ expense/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.views import View
+from django.shortcuts import render, redirect
 
-def home(request):
-    return HttpResponse("Hello, world! This is the home page.")
+from expense.forms import UserCreationForm
+
+
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        context = {
+            'form': UserCreationForm()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()  # Сохраняем пользователя
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            # Аутентифицируем пользователя
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)  # Логиним пользователя
+                return redirect('home')
+
+            # Если аутентификация не удалась, возвращаем ошибку
+            form.add_error(None, "Authentication failed. Please try again.")
+
+        # Если форма невалидна, возвращаем её с ошибками
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context)
